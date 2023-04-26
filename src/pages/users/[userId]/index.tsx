a2c4c8a63f9110500ae6a999a5a1d99debf67fc4
oleglukasevici <ardorfire0@gmail.com>
2023-04-26 11:49:38 -0300

    fix: add loading to follow button

--- src/pages/users/[userId]/index.tsx
+++ src/pages/users/[userId]/index.tsx
@@ -248,7 +248,7 @@ const UserPage: NextPage<
           />
           <ShouldRender if={!loggedUserIsProfileOwner && session?.user?.id}>
             <Button
-              disabled={loadingUser}
+              loading={loadingUser}
               variant="gradient"
               onClick={handleClickFollowButton}
               absolute
